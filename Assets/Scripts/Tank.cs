9064874a6c85f29707d4fbb6901fd5564b95ee4e
NUCBOX_M5PLUS\ooshiro <yanbarumajimun@ryukyugame.page>
2026-04-27 11:33:53 +0900

    Add Project.

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Tank : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float turnSpeed = 90f;
+    public float topTurnSpeed = 90f;
+    public float cannonTurnSpeed = 90f;
+
+    public Transform top;
+    public Transform cannon;
+
+    [Header("** Shot Settings **")]
+    public Transform shotPoint;         // 총알이 발사되는 위치
+    public GameObject bulletPrefab;     // 발사할 총알 프리팹
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Turn();
+        TopTurn();
+        CannonTurn();
+        Shot();
+    }
+
+    void Movement()
+    {
+        float deltaMoveSpeed = moveSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, deltaMoveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -deltaMoveSpeed);
+        }
+    }
+
+    void Turn()
+    {
+        float deltaTurnSpeed = turnSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -deltaTurnSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, deltaTurnSpeed, 0);
+
+        }
+    }
+
+    void TopTurn()
+    {
+        float deltaTopTurnSpeed = topTurnSpeed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            top.transform.Rotate(0, -deltaTopTurnSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            top.transform.Rotate(0, deltaTopTurnSpeed, 0);
+        }
+    }
+
+    void CannonTurn()
+    {
+        float deltaCannonTurnSpeed = cannonTurnSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            cannon.transform.Rotate(-deltaCannonTurnSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            cannon.transform.Rotate(deltaCannonTurnSpeed, 0, 0);
+        }
+    }
+
+    void Shot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 弾丸を生成する
+            GameObject origin   = bulletPrefab;
+            Vector3 position    = shotPoint.position;
+            Quaternion rotation = shotPoint.rotation;
+
+            GameObject bullet = Instantiate(origin, position, rotation);
+
+            // 弾丸を飛ばす
+            //Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            //rb.AddForce();
+        }
+    }
+}
